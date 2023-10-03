d796dc08ee54c10c33d079b99da7582a372e70d6
jaxe93 <jaxe93work@gmail.com>
2023-10-02 23:03:23 -0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Module-06-weather-App
+Weather Dashboard Project README
+Table of Contents
+Introduction
+Features
+Installation
+Usage
+Screenshots
+Introduction
+Provide a brief introduction to your Weather Dashboard project. Explain its purpose and what it does. You can also mention any goals or objectives of the project.
+
+Features
+List the key features of your Weather Dashboard. Highlight what makes it unique or useful. For example:
+
+Real-time weather data retrieval
+Display of current weather conditions, forecasts, and other relevant information
+User-friendly interface
+Location-based weather updates
+Installation
+Explain how users can install or set up your Weather Dashboard. Include any prerequisites or dependencies they need to have installed beforehand. Provide clear, step-by-step instructions. For example:
+
+Clone this repository: git clone https://github.com/yourusername/weather-dashboard.git
+Navigate to the project directory: cd weather-dashboard
+Install dependencies: npm install
+Usage
+Describe how users can use your Weather Dashboard. Provide examples of common use cases and any necessary instructions. For example:
+
+Open the application in your web browser.
+Enter the name of the location you want to check the weather for in the search bar.
+Click the "Search" button to retrieve weather data.
+View the current weather conditions, forecasts, and other information on the dashboard.
+Screenshots
+Include screenshots or images that showcase your Weather Dashboard. You can use this section to visually demonstrate the features and user interface of your project.
+
+Feel free to customize the content and formatting to match your project's specifics. This revised README template should provide users with a clear understanding of your Weather Dashboard Project without including the license and contributing sections.
---
+++ asset.js
@@ -0,0 +1,131 @@
+var dummyData;
+
+//function to get history out of local storage.
+
+getHistoty()
+if (!window.localStorage.getItem("cityHistory")) {
+    
+    window.localStorage.setItem("cityHistory",JSON.stringify({})) 
+}
+
+// emojis that will populate corrisponding to what kind of weather is happening, as found in the API
+
+var emojis= {
+    Clouds: "&#9729;",
+    Clear: "&#9728;",
+    Rain: "&#127783;",
+    Thunder: "&#127785;",
+    Snow: "&#127784;",
+}
+
+//cityname, date, temp, humidity, and emoji that goes with information that is pulled from API
+
+var data= [
+   {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    }, 
+    {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    },
+    {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    },
+    {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    },
+    {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    },
+    {
+        CityName: undefined, Date: undefined, Temp: undefined, Humidity: undefined, Wind: undefined, Weather: undefined,
+    },
+]
+
+//function for getting history and then building button to click on to retrieve from local storage.
+
+function getHistoty() {
+    var hist = JSON.parse(window.localStorage.getItem ("cityHistory"))
+    var HTML = ""
+    for (var key in hist) {
+        HTML += `
+        <li><button onclick="search('${hist[key]}')">${hist[key]} </button></li>
+        `
+    }
+    document.getElementById("cityHistory").innerHTML = HTML
+}
+
+//function for building my forcast
+
+var weather; 
+function build() {
+document.getElementById("forcasts").removeAttribute("hidden")    
+dummyData = weather;
+data[0].CityName = dummyData.city.name
+var hist = JSON.parse(window.localStorage.getItem ("cityHistory"))
+hist[dummyData.city.name] = dummyData.city.name
+window.localStorage.setItem ("cityHistory",JSON.stringify(hist))
+data[0].Date = dummyData.list[0].dt_txt.slice(0,10)
+data[0].Temp = (dummyData.list[0].main.temp*9/5-459.67 + "").slice(0,4)
+data[0].Humidity = dummyData.list[0].main.humidity
+data[0].Wind = dummyData.list[0].wind.speed
+data[0].Weather = dummyData.list[0].weather[0].main
+var j = 1
+for (var i= 0; i < dummyData.list.length; i++) {
+    
+    if (data[0].Date === dummyData.list[i].dt_txt) {continue;}
+    if (dummyData.list[i].dt_txt.includes("18:00:00")) {
+        data[j].Date = dummyData.list[i].dt_txt.slice(0,10)
+        data[j].Temp = (dummyData.list[i].main.temp*9/5-459.67 + "").slice(0,4)
+        data[j].Humidity = dummyData.list[i].main.humidity
+        data[j].Wind = dummyData.list[i].wind.speed
+        data [j].Weather = dummyData.list[i].weather[0].main 
+        
+        j++;
+    }
+}
+
+//var date0= dummyData.list[0].dt_txt cant remember why I made this.  
+
+for (var i= 0; i < data.length; i++) {
+    document.getElementById("tempData"+i).innerHTML = "Temprature. " + data[i].Temp
+    document.getElementById("humidityData"+i).innerHTML = "Humidity " + data[i].Humidity
+    document.getElementById("windData"+i).innerHTML = "Wind Speed " + data[i].Wind
+    document.getElementById("date"+i).innerHTML = "Date " + data[i].Date
+    document.getElementById("emoji"+i).innerHTML = emojis[data[i].Weather]
+}
+document.getElementById("cityName").innerHTML = "City: " + data[0].CityName
+}
+
+//function for getting longitude and latitude from City name in input.
+
+async function getLonLat(cityName) {
+    var apiKey = "261a264429697f4cacc29d41aac775ac"
+    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+    return (await fetch(url)).json();
+
+}
+
+//fuction that inserts lat long, and api key into link for the 5 day forcast
+
+async function getWeather(data) {
+    console.log(data)
+    var Lat = data[0].lat
+    var Lon = data[0].lon
+    var apiKey = "261a264429697f4cacc29d41aac775ac"
+    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${Lat}&lon=${Lon}&appid=${apiKey}`
+    return (await fetch(url)).json();
+    
+}
+
+function search(cityName = document.getElementById("searchBar").value) {
+    getLonLat(cityName)
+        .then((data) => getWeather(data))
+        .then((data) => {
+            weather = data;
+            console.log(weather)
+            build()
+            getHistoty()
+        })
+        .catch((err) => console.log(err))
+}
+
